38ee82ed31bc407ad7950a58f3ed8c2e9705ce1f
spacecreator8 <spcc888@ya.ru>
2024-05-16 11:37:30 +0700

    добавил маршрут API

--- routes/api.php
+++ routes/api.php
@@ -20,15 +20,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group([
-
     'middleware' => 'api',
     'prefix' => 'auth'
-
 ], function ($router) {
-
     Route::post('login', 'App\Http\Controllers\AuthController@login');
     Route::post('logout', 'App\Http\Controllers\AuthController@logout');
     Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
     Route::post('me', 'App\Http\Controllers\AuthController@me');
 
+});
+
+Route::group(['namespace'=>'App\Http\Controllers\User', 'prefix'=>'user', 'middleware'=>'jwt.auth'], function(){
+    Route::post('/store', 'StoreController');
 });
